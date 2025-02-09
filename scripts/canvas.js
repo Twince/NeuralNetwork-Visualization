e7399e07b911b1dea06476b2b72ce8afb8ecb2d7
Sunwoo Ha <tjsdn811@gmail.com>
2025-02-09 14:37:26 +0900

    chore: add basic tempate

---
+++ scripts/canvas.js
@@ -0,0 +1,6 @@
+// if (modernizr-custom.canvas) {
+//     console.log('Canvas를 지원하는 브라우저');
+// } 브라우저가 Canvas를 지원하는지 여부 확인
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
