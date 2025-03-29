3f0524016482d7e1b5158b86d9e7e86cb94505cc
Sunwoo Ha <tjsdn811@gmail.com>
2025-03-29 20:48:26 +0900

    feat: 활성화 함수

---
+++ src/core/utils/activationFunctoins.js
@@ -0,0 +1,14 @@
+const activationFunction = (v) => {
+    const value = sigmoid(v);//활성화 함수
+    return parseFloat(value.toFixed(5));//활성화 함수를 적용시킨 값을 소숫점 5자리로 반올림
+}
+
+const sigmoid = ($x) => {
+    return 1 / (1 + Math.exp($x));
+};
+
+const ReLU = ($x) => {
+    return Math.max(0, $x);
+}
+
+export default activationFunction;
