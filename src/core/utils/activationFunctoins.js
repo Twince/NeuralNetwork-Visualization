2207d29e3f56d3d73874a3e9a6092fcf1f736e93
Sunwoo Ha <tjsdn811@gmail.com>
2025-03-30 00:59:14 +0900

    refactor: 오류 수정 및 리팩토링

--- src/core/utils/activationFunctoins.js
+++ src/core/utils/activationFunctoins.js
@@ -1,10 +1,12 @@
-const activationFunction = (v) => {
-    const value = sigmoid(v);//활성화 함수
-    return parseFloat(value.toFixed(5));//활성화 함수를 적용시킨 값을 소숫점 5자리로 반올림
+const activationFunction = (list) => {
+    return list.map(v => {
+        const value = sigmoid(v)
+        return parseFloat(value.toFixed(5)) //활성화 함수를 적용시킨 값을 소숫점 5자리로 반올림
+    })
 }
 
 const sigmoid = ($x) => {
-    return 1 / (1 + Math.exp($x));
+    return 1 / (1 + Math.exp(-$x));
 };
 
 const ReLU = ($x) => {
--- src/core/utils/matrixUtils.js
+++ src/core/utils/matrixUtils.js
@@ -1,7 +1,11 @@
-export const createRandomMatrix = (rows, cols, min = 1, max = 999999) =>
-    Array.from({ length: rows }, () =>
-        Array.from({ length: cols }, () =>  {
-            const value =  Math.floor((Math.random() * (max - min + 1)) + min)/1000000-0.5;
+export const createRandomWeight = (pre, post) =>
+    Array.from({ length: pre }, () =>
+        Array.from({ length: post }, () =>  {
+            const value = (Math.random() - .5) * 1.99999;
             return parseFloat(value.toFixed(5));
         })
     );
+
+export const matrixMultiplication = (matrix1, matrix2) => {
+
+}
