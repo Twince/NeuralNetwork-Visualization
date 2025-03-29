70cb3d3eda770718bf5314d0f622c878df1ccc4d
Sunwoo Ha <tjsdn811@gmail.com>
2025-03-29 20:47:05 +0900

    feat(WIP): 오타 수정

--- src/core/neuralNetwork.js
+++ src/core/neuralNetwork.js
@@ -1,10 +1,16 @@
+import activationFunction from "./utils/activationFunctoins.js";
+import { createRandomMatrix } from "./utils/matrixUtils.js";
+
 class neuralNetwork {
     constructor({inputNodes,hiddenNodes, outputNodes, learningRate}) {
         this.inputnodes = inputNodes;
         this.hiddenNodes = hiddenNodes;
-        this.OutputNodes = outputNodes;
+        this.outputNodes = outputNodes;
 
         this.learningRate = learningRate;
+
+        this.W_inputTohidden = (createRandomMatrix(3, 3));
+        this.W_hiddenToOutput = (createRandomMatrix(3, 3));
     }
 
     train(){
