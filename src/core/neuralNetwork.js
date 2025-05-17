6d812115a8935cd56f85c25ac526f446b002c84a
Sunwoo Ha <tjsdn811@gmail.com>
2025-05-18 01:14:18 +0900

    feat: 전치행렬 및 학습에 대한 로직 추가

--- src/core/neuralNetwork.js
+++ src/core/neuralNetwork.js
@@ -1,5 +1,5 @@
 import activationFunction from "./utils/activationFunctoins.js";
-import { createRandomWeight, matrixMultiply } from "./utils/matrixUtils.js";
+import { createRandomWeight, matrixMultiply,transposeMatrix } from "./utils/matrixUtils.js";
 
 class neuralNetwork {
     constructor({inputNodes,hiddenNodes, outputNodes, learningRate}) {
@@ -22,7 +22,14 @@ class neuralNetwork {
         const final_inputs = matrixMultiply(hidden_outputs, this.W_hiddenToOutput)
         const final_outputs = activationFunction(final_inputs);
 
-        const error
+        const output_error = targets - final_outputs;
+        const hidden_error = matrixMultiply(transposeMatrix(this.W_hiddenToOutput), output_error);
+
+        const W_hiddenToOutput_derivative = matrixMultiply(((output_error * final_outputs) * (1.0 - final_outputs)), transposeMatrix(hidden_outputs));
+        this.W_hiddenToOutput += W_hiddenToOutput_derivative.map(array => array.map(v => v * this.learningRate));
+
+        const W_inputToHidden_derrivative = matrixMultiply(((hidden_error * hidden_outputs) * (1.0 - hidden_outputs)), transposeMatrix(inputs));
+        this.W_hiddenToOutput += W_inputToHidden_derrivative.map(array => array.map(v => v * this.learningRate));
     }
 
     query(inputs){
@@ -30,9 +37,10 @@ class neuralNetwork {
         const hidden_outputs = activationFunction(hidden_inputs); // 은닉계층에서 출력되는 신호계산(활성화 함수)
         const final_inputs = matrixMultiply(hidden_outputs, this.W_hiddenToOutput) // 출력계층으로 들어가는 입력값과 가중치 행렬곱
         const final_outputs = activationFunction(final_inputs); // 출력계층에서 나가는 최종 계산결과 return
+        console.log(final_inputs);
         return final_outputs;
     }
 }
 
 const $NN = new neuralNetwork({inputNodes: 3, hiddenNodes: 3, outputNodes: 3 ,learningRate: 0.2});
-$NN.query([[0.99, 0.5, -0.4],[0.15, 0.21, -0.11], [-0.89, -0.99, 1.00]]);
+$NN.query([0.99, 0.5, -0.4]);
