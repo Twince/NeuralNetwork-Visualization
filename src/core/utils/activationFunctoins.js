3f1b92ae86a4e4c5d671c9bc7a55ae452ce572a1
Sunwoo Ha <tjsdn811@gmail.com>
2025-05-17 23:25:42 +0900

    refactor: 활성화 함수 리팩토링

--- src/core/utils/activationFunctoins.js
+++ src/core/utils/activationFunctoins.js
@@ -1,9 +1,10 @@
-const activationFunction = (list) => {
-    return list.map(v => {
-        const value = sigmoid(v)
-        return parseFloat(value.toFixed(5)) //활성화 함수를 적용시킨 값을 소숫점 5자리로 반올림
-    })
-}
+const activationFunction = matrix =>
+    matrix.map(array =>
+        array.map(v => {
+            const value = sigmoid(v)
+            return parseFloat(value.toFixed(5)) //활성화 함수를 적용시킨 값을 소숫점 5자리로 반올림
+        })
+    )
 
 const sigmoid = ($x) => {
     return 1 / (1 + Math.exp(-$x));
