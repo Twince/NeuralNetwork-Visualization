213f5b0ef8a30bc54677e60962f5fb6e450946ad
Sunwoo Ha <tjsdn811@gmail.com>
2025-05-22 20:44:22 +0900

    fix: 가중치가 업데이트 되지 않던 문제 해결

--- src/core/neuralNetwork.js
+++ src/core/neuralNetwork.js
@@ -19,27 +19,22 @@ export class NeuralNetwork {
     train(inputs, targets){
         const hidden_inputs = matrixMultiply(this.W_inputToHidden, inputs.map(v => [v])); //신경망 입력 노드의 출력 결과를 Nx1 형태의 행렬곱으로 변환.
         const hidden_outputs = activationFunction(hidden_inputs);
-        console.log("acti:", hidden_outputs);
         const final_inputs = matrixMultiply(this.W_hiddenToOutput, hidden_outputs);
-        console.log("final:", final_inputs);
         const final_outputs = activationFunction(final_inputs);
 
         const output_errors = targets.map((v, idx) => v - final_outputs[idx]).map(v => [v]); // 출력 계층의 오차를 목표값 - 출력값으로 지정
-        console.log("output_erros:", output_errors);
         const hidden_errors = matrixMultiply(transposeMatrix(this.W_hiddenToOutput), output_errors); // 은닉 계층의 오차를 은닉-> 출력 계층의 가중치값과(W_hiddenToOutput.T) 출력 계층의 오차들을 재조합하여 계산
-        console.log("hidden_errors:", hidden_errors);
 
         const activationDerivative_HtO = final_outputs.map(v => 1.0 - v).map((v, idx) => v * final_outputs[idx]); // hidden to output derivative
         const outputGradient = activationDerivative_HtO.map((v, idx) => v * output_errors[idx]).map(v => [v]);
-        console.log("outputGradient", outputGradient);
-        console.log("hiddenOutput.length", hidden_outputs.length);
-        this.W_hiddenToOutput = matrixMultiply(outputGradient, transposeMatrix(hidden_outputs)).map(array => array.map(v => v * this.learningRate)); // 오차값을 이용해 은닉 계층과 출력 계층간의 가중치 업데이트
-        console.log("updated: W_hiddenToOutput", this.W_hiddenToOutput);
+        const W_HtO_Update = matrixMultiply(outputGradient, transposeMatrix(hidden_outputs)).map(array => array.map(v => v * this.learningRate)); // 오차값을 이용해 은닉 계층과 출력 계층간의 가중치 업데이트
+        this.W_hiddenToOutput = this.W_hiddenToOutput.map((row, i)=> row.map((v, j) => v + W_HtO_Update[i][j]));
 
         const activationDerivative_ItH = hidden_outputs.map(v => 1.0 - v).map((v, idx) => v * hidden_outputs[idx]);
         const hiddenGradient = activationDerivative_ItH.map((v, idx) => v * hidden_errors[idx]).map(v => [v]);
-        console.log("hiddenGradient" ,hiddenGradient);
-        this.W_inputToHidden = matrixMultiply(hiddenGradient, transposeMatrix(inputs.map(v => [v]))).map(array => array.map(v => v * this.learningRate));
+
+        const W_ItH_update = matrixMultiply(hiddenGradient, transposeMatrix(inputs.map(v => [v]))).map(array => array.map(v => v * this.learningRate));
+        this.W_inputToHidden = this.W_inputToHidden.map((row, i)=> row.map((v, j) => v + W_ItH_update[i][j]));
         console.log("updated: W_inputToHidden", this.W_inputToHidden);
     }
 
@@ -52,4 +47,4 @@ export class NeuralNetwork {
     }
 }
 
-export const networkInfo = {inputNodes: 784, hiddenNodes: 150, outputNodes: 10, learningRate: 0.2};
+export const networkInfo = {inputNodes: 784, hiddenNodes: 150, outputNodes: 10, learningRate: 0.1};
