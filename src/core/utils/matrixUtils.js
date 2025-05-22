1719687d6903a47223ef937c1d4e425e6a6afbcb
Sunwoo Ha <tjsdn811@gmail.com>
2025-05-22 20:43:57 +0900

    chore: 로깅 데이터 주석화

--- src/core/utils/matrixUtils.js
+++ src/core/utils/matrixUtils.js
@@ -2,14 +2,15 @@ export const createRandomWeight = (col, row) =>
     Array.from({ length: col }, () =>
         Array.from({ length: row }, () =>  {
             const value = (Math.random() - .5) * 1.99999;
-            return parseFloat(value.toFixed(5));
+            // return parseFloat(value.toFixed(5));
+            return value;
         })
     );
 
 export const matrixMultiply = (A, B) => {
     A = Array.isArray(A[0]) ? A : [A];
-    console.log("A:",A, "B:", B);
-    console.log("A[0].length:",A[0].length, "B.length:", B.length);
+    // console.log("A:",A, "B:", B);
+    // console.log("A[0].length:",A[0].length, "B.length:", B.length);
     if (!Array.isArray(A) || !Array.isArray(B)) throw new Error('matrixA, B must be an array');
     else if (A[0].length !== B.length) throw new Error('rows and columns length doesn\'t match');
     return A.map((row, i) =>
