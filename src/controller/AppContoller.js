68eb1fbf21e53efb139d25d1e6b651290cd112ed
Sunwoo Ha <tjsdn811@gmail.com>
2025-05-31 19:03:25 +0900

    feat: 라이프 사이클 명로화 및 entry point 지정

---
+++ src/controller/AppContoller.js
@@ -0,0 +1,28 @@
+import WeightManager from "../core/WeightManager.js";
+import NeuralNetworkBase from "../core/NeuralNetworkBase.js";
+import InputCanvasHandler from "../view/InputCanvasHandler.js";
+
+import eventBus from "./EventBus.js";
+import { DATA_EVENTS, HANDLER_EVENTS } from "./constants/events.js";
+import { NETWORK_CONFIG } from "./constants/networkConfig.js";
+
+class AppController {
+    constructor({ dataStore }) {
+        this.dataStore = dataStore;
+    }
+
+    async initialize() {
+        console.log("Initializing...WeightManager");
+        const $WM = new WeightManager(NETWORK_CONFIG);
+        console.log("Initializing...NeuralNetworkBase");
+        const $NN = new NeuralNetworkBase(await $WM.getWeights());
+        console.log("Initializing...InputCanvasHandler");
+        const $ICN = new InputCanvasHandler();
+        console.log("Initializing Handler: complete!");
+        eventBus.emit(HANDLER_EVENTS.NN_INITIALIZE, $NN);
+        eventBus.emit(HANDLER_EVENTS.ICH_INITIALIZE, $ICN);
+        eventBus.emit(HANDLER_EVENTS.APP_READY, this.dataStore);
+    };
+}
+
+export default AppController;
