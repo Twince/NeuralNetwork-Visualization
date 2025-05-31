f7444361f7f0ec4487f4272a7e591648d9719d9a
Sunwoo Ha <tjsdn811@gmail.com>
2025-05-31 19:07:38 +0900

    perf: loose coupling및 source of truth이벤트 정의를 위한 enum-like 문서화

---
+++ src/controller/constants/events.js
@@ -0,0 +1,14 @@
+export const DATA_EVENTS = Object.freeze({
+    QUERY_UPDATE: 'query:update',
+    QUERY_CHANGED: 'query:changed',
+    NODE_UPDATE: 'node:update',
+    NODE_CHANGED: 'node:changed',
+    RESULT_UPDATE: 'result:update',
+    RESULT_CHANGED: 'result:changed'
+});
+
+export const HANDLER_EVENTS = Object.freeze({
+    APP_READY: 'app: ready',
+    NN_INITIALIZE: 'nn: initialize',
+    ICH_INITIALIZE: 'ich: initialize',
+})
