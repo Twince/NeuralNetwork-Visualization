11bbbcce3ade73b354528c96d082a966a773dcaa
Sunwoo Ha <tjsdn811@gmail.com>
2025-06-07 23:27:15 +0900

    chore: 이름을 명시적으로 수정

--- src/controller/AppContoller.js
+++ src/controller/AppContoller.js
@@ -1,10 +1,12 @@
 import WeightManager from "../core/WeightManager.js";
 import NeuralNetworkBase from "../core/NeuralNetworkBase.js";
-import UserInputHandler from "../view/components/userInput/userInputHandler.js";
+import UserInputCanvas from "../view/components/userInput/UserInputCanvas.js";
+import { DataStore } from "./DataStore.js";
 
 import eventBus from "./EventBus.js";
 import { DATA_EVENTS, HANDLER_EVENTS } from "./constants/events.js";
 import { NETWORK_CONFIG } from "./constants/networkConfig.js";
+import processingCanvas from "../view/components/userInput/ProcessingCanvas.js";
 
 class AppController {
     constructor({ dataStore }) {
@@ -16,12 +18,12 @@ class AppController {
         const $WM = new WeightManager(NETWORK_CONFIG);
         console.log("Initializing...NeuralNetworkBase");
         const $NN = new NeuralNetworkBase(await $WM.getWeights());
-        console.log("Initializing...UserInputHandler");
-        const $UIH = new UserInputHandler('userInputCanvas');
-        $UIH.mount();
+        const $DS = new DataStore();
+        console.log("Initializing...UserInputCanvas");
+        const $UIH = new UserInputCanvas('userInputCanvas');
         console.log("Initializing Handler: complete!");
         eventBus.emit(HANDLER_EVENTS.NN_INITIALIZE, $NN);
-        eventBus.emit(HANDLER_EVENTS.ICH_INITIALIZE, $ICN);
+        eventBus.emit(HANDLER_EVENTS.ICH_INITIALIZE, $UIH);
         eventBus.emit(HANDLER_EVENTS.APP_READY, this.dataStore);
     };
 }
