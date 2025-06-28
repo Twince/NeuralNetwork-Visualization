90b3d13c0d6d09e218429f43273480f1b53d18fd
Sunwoo Ha <tjsdn811@gmail.com>
2025-06-29 01:37:28 +0900

    chore: eventBus 로그 추가

--- src/controller/EventBus.js
+++ src/controller/EventBus.js
@@ -2,8 +2,9 @@ const EventBus = {
     _events: {},
 
     on(eventName, subscriber) {
-        if(!this._events[eventName]) return this._events[eventName] = [];
+        if(!this._events[eventName]) this._events[eventName] = [];
         this._events[eventName].push(subscriber);
+        console.log("📌 [eventBus] 등록됨:", eventName);
     },
 
     off(eventName, subscriber) {
@@ -13,6 +14,7 @@ const EventBus = {
 
     emit(eventName, payload) {
         if(!this._events[eventName]) return;
+        console.log("📡 [eventBus] emit 발생:", eventName, payload);
         this._events[eventName].forEach(fn => fn(payload));
     }
 }
