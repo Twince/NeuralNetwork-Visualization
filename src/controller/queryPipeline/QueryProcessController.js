1922998e5a886c07f311473f2f744fce8be8904e
Sunwoo Ha <tjsdn811@gmail.com>
2025-07-20 23:49:29 +0900

    feat: 입력 및 질의 성능 관리를 위한 throttling 추가

--- src/controller/queryPipeline/QueryProcessController.js
+++ src/controller/queryPipeline/QueryProcessController.js
@@ -1,50 +1,47 @@
 import eventBus from "../EventBus.js";
 import { DATA_EVENTS, HANDLER_EVENTS, DRAWING_EVENTS } from "../constants/events.js";
 import DrawingEventHandler from "./DrawingEventHandler.js";
-import UserInputCanvas from "../../view/components/userQuery/UserInputCanvas.js";
-import PathTrackingCanvas from "../../view/components/userQuery/PathTrackingCanvas.js";
 import {pixelExtractor} from "../queryPipeline/pixelExtractor.js";
+import {throttle} from "../queryPipeline/throttle.js";
 
 
 class QueryProcessController {
     constructor({ userInputCanvas, trackingCanvas, alignCanvas, resizeCanvas, $NN }) {
+        const drawingEventHandler = new DrawingEventHandler(); // QueryProcessController와 1:1 관계를 위해 강한결합(strong coupling)
         this.userInputCanvas = userInputCanvas;
         this.trackingCanvas = trackingCanvas;
         this.alignCanvas = alignCanvas;
         this.resizeCanvas = resizeCanvas;
 
         this.$NN = $NN;
-        this.drawingEventHandler = new DrawingEventHandler();
         this.drawingEvent();
-
-        this.alignCanvas.setupCanvas();
-        // this.drawingEventHandler.registerEvents(userInputCanvas);
-        console.log("이벤트 받음.");
-        eventBus.on(HANDLER_EVENTS.NN_INITIALIZE, (nn) => this.$NN = nn);
     }
 
     drawingEvent() {
-        console.log("QueryProcessController: drawingEvent 실행됨.");
+        const throttleQuery = throttle((inputs) => {
+            const result = this.$NN.query(inputs);
+            if(result) {
+                eventBus.emit(DATA_EVENTS.RESULT_CHANGED, result);
+                // Array 중 가장 값이 큰 값의 index number가 추론 결과
+            }
+        },100);
+
         eventBus.on(DRAWING_EVENTS.START_DRAW, ({x, y}) => {
-            console.log("eventBus:on 실행(startDraw)");
             this.userInputCanvas.startPath(x, y);
             this.trackingCanvas.startPath(x, y);
-
         });
         eventBus.on(DRAWING_EVENTS.DRAW, ({x, y}) => {
-            console.log("eventBus:on 실행(Drawing)");
             this.userInputCanvas.drawPath(x, y);
             this.trackingCanvas.drawPath(x, y);
             this.alignCanvas.updateCanvasScale();
             this.alignCanvas.centralize(this.trackingCanvas.canvas);
             this.resizeCanvas.downScale(this.alignCanvas.canvas);
-            console.log("$NN: ", this.$NN.query(pixelExtractor(this.resizeCanvas)));
+            throttleQuery(pixelExtractor(this.resizeCanvas));
         });
         eventBus.on(DRAWING_EVENTS.END_DRAW, () => {
             this.userInputCanvas.endPath();
             this.trackingCanvas.endPath();
         });
-        console.log("구독이 완료됨.");
     }
 }
 
