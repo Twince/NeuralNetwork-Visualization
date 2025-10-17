448de7111cf7579b9a6e255b150f49afd7feffe4
Twince <tjsdn811@gmail.com>
2025-10-17 16:49:00 +0900

    Revert "WIP: 아키텍처 변경에 따른 리팩토"
    This reverts commit 51ec2e5e447e371f949e1e744d1e5994f07ae12e.

--- src/controller/perceptron/PerceptronController.ts
+++ src/controller/perceptron/PerceptronController.ts
@@ -2,31 +2,93 @@ import { IPerceptronControllerProps } from '@/controller/perceptron/types/Percep
 import { NodeHandler } from '@/controller/perceptron/NodeHandler.ts';
 import { EdgePositionHandler } from '@/controller/perceptron/EdgeHandler.ts';
 import { ScrollEventHandler } from '@/controller/perceptron/ScrollEventHandler.ts';
-import { DATA_EVENTS, DataEvent } from '@/controller/constants/events.ts';
+import { DATA_EVENTS, DataEvent, SCROLL_EVENTS } from '@/controller/constants/events.ts';
 import eventBus from '@/controller/EventBus.ts';
 import { eventPayloads } from '@/controller/types/eventBus.ts';
 import { compressArr } from '@/controller/perceptron/utils/compressArr.ts';
 
+import { networkConfig } from '@/controller/constants/networkConfig.ts';
+import { normalizeNetworkConfig } from '@/controller/perceptron/utils/normalizeNetworkInfo.ts';
+
+import { RENDERER_CONFIG } from '@/controller/constants/rendererConfig.ts';
+const { rotationDelta, degree, displayNodes, scrollDivider } = RENDERER_CONFIG;
 import BaseCanvas from '@/view/components/perceptron/BaseCanvas.ts';
 
 class PerceptronController {
-    private nodePositionHandler: NodeHandler;
-    private edgePositionHandler: EdgePositionHandler;
-    private scrollEventHandler: ScrollEventHandler;
+    private NodeHandler: INodeHandler;
+    private EdgeHandler: IEdgePositionHandler;
+    private BaseCanvas: IBaseCanvas;
+    private ScrollEventHandler: IScrollEventHandler;
+
+    private normalizedNetworkInfo: any;
 
     constructor({
-        nodeRenderer,
-        nodePositionHandler,
-        edgePositionHandler,
-        scrollEventHandler,
+        NodeRenderer,
+        NodeHandler,
+        EdgeHandler,
+        BaseCanvas,
+        ScrollEventHandler,
     }: IPerceptronControllerProps) {
-        this.nodePositionHandler = nodePositionHandler;
-        this.edgePositionHandler = edgePositionHandler;
-        this.scrollEventHandler = scrollEventHandler;
+        this.NodeHandler = NodeHandler;
+        this.EdgeHandler = EdgeHandler;
+        this.BaseCanvas = BaseCanvas;
+        this.ScrollEventHandler = ScrollEventHandler;
+
+        this.initializeNodeValue();
+
         eventBus.on(DATA_EVENTS.NODE_CHANGED, ({ inputs, hiddenOutputs, finalOutputs }) => {
             console.log('nodes 출력:', inputs, compressArr(inputs), hiddenOutputs, finalOutputs);
         });
     }
+
+    initializeNodeValue() {
+        this.normalizedNetworkInfo = normalizeNetworkConfig(networkConfig); // 원활한 시각화를 위해 입력 레이어의 크기를 compress
+        this.NodeHandler.initializeNode(this.normalizedNetworkInfo);
+    }
+
+    registerScrollEvent() {
+        eventBus.on(SCROLL_EVENTS.SCROLL_CHANGED, (mouseScroll) =>
+            this.calculateNodePosition(mouseScroll, 0),
+        );
+        eventBus.on(SCROLL_EVENTS.TOUCH_CHANGED, (touchScroll) =>
+            this.calculateNodePosition(0, touchScroll),
+        );
+    }
+
+    calculateNodePosition(mouseScroll: number, touchScroll: number) {
+        Object.keys(this.normalizedNetworkInfo).map((key: string, index: number) => {
+            this.BaseCanvas.rotateCanvas(
+                true,
+                degree * 90 - (degree * rotationDelta * this.normalizedNetworkInfo[key]) / 2,
+            );
+            this.BaseCanvas.saveState();
+            const layerSize = this.normalizedNetworkInfo[key];
+
+            const scrollOffset = (mouseScroll + touchScroll) / scrollDivider;
+            const displayStart = layerSize / 2 - displayNodes / 2 + scrollOffset;
+            const displayEnd = layerSize / 2 + displayNodes / 2 + scrollOffset;
+
+            const displayCondition = i >= displayStart && i < displayEnd;
+
+            Array.from({ length: layerSize }, (_: unknown, i: number) => i).map((i) => {
+                if (displayCondition) {
+                    this.nodeRenderer.drawNode(size[index], this.nodeObjectSet[key][i].getValue());
+                    this.drawText(i);
+                    this.BaseCanvas.rotateCanvas(true, degree * rotationDelta);
+                } else {
+                    this.BaseCanvas.rotateCanvas(true, degree * rotationDelta);
+                }
+            });
+        });
+
+        // Position, Rotation 값 Object로 넘기기
+    }
+
+    calculateEdgePosition() {
+        // Position 값 Object로 넘기기
+    }
+
+    renderPerceptron() {}
 }
 
 export default PerceptronController;
--- src/controller/perceptron/types/PerceptronController.ts
+++ src/controller/perceptron/types/PerceptronController.ts
@@ -1,6 +1,7 @@
 export interface IPerceptronControllerProps {
-    nodePositionHandler: INodePositionHandler;
-    edgePositionHandler: INdgePositionHandler;
-    nodeRenderer: INodeRenderer;
-    scrollEventHandler: IScrollEventHandler;
+    NodeHandler: INodePositionHandler;
+    EdgeHandler: INdgePositionHandler;
+    NodeRenderer: INodeRenderer;
+    BaseCanvas: IBasecanvas;
+    ScrollEventHandler: IScrollEventHandler;
 }
