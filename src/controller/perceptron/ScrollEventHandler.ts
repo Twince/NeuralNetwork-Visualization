fa4294ed2c7a6ff808c1e1b54b54d5bc82080e83
Sunwoo Ha <tjsdn811@gmail.com>
2025-10-25 19:33:47 +0900

    feat: 노드 수에 따른 스크롤 한계값 구현

--- src/controller/perceptron/ScrollEventHandler.ts
+++ src/controller/perceptron/ScrollEventHandler.ts
@@ -1,9 +1,13 @@
 import { RENDERER_CONFIG } from '@/controller/constants/rendererConfig.ts';
+import { NETWORK_CONFIG } from '@/controller/constants/networkConfig.ts';
 
 import eventBus from '@/controller/EventBus.ts';
 import { SCROLL_EVENTS } from '@/controller/constants/events.ts';
+import { normalizeNetworkConfig } from '@/controller/perceptron/utils/normalizeNetworkInfo.ts';
+import { findWidestLayer } from '@/controller/perceptron/utils/findWidestLayer.ts';
 
-const { degree, rotationDelta, scrollDivider } = RENDERER_CONFIG;
+const { degree, rotationDelta, displayNodes, scrollDivider } = RENDERER_CONFIG;
+const { inputNodes, hiddenNodes, outputNodes } = NETWORK_CONFIG;
 
 class ScrollEventHandler {
     private BaseCanvas: IBaseCanvas;
@@ -14,6 +18,12 @@ class ScrollEventHandler {
     private touchDirection: number;
     private rotationStack: number;
 
+    private widestLayer: number;
+    private leftLimit: number;
+    private rightLimit: number;
+    private leftScrollLimit: boolean;
+    private rightScrollLimit: boolean;
+
     constructor(BaseCanvas: IBaseCanvas) {
         this.BaseCanvas = BaseCanvas;
         this.canvasEl = BaseCanvas.getCanvas(); // perceptron base canvas
@@ -22,23 +32,38 @@ class ScrollEventHandler {
         this.touchMove = 0;
         this.touchDirection = 0;
 
+        this.widestLayer = 0;
+        this.leftScrollLimit = true;
+        this.rightScrollLimit = true;
+
         this.registerEvent();
     }
 
     registerEvent() {
+        this.calculateScrollLimit();
         this.handleMouseScroll();
         this.handleTouchMove();
     }
 
+    calculateScrollLimit(): void {
+        const normalizedNetworkInfo = normalizeNetworkConfig(NETWORK_CONFIG);
+        const { value: widestLayer } = findWidestLayer(normalizedNetworkInfo); // 스크롤 최대치 계산을 위한 가장 큰 노드의 폭을 계산
+        this.rightLimit = (widestLayer / 2 - displayNodes) * scrollDivider;
+        this.leftLimit = -this.rightLimit;
+    }
+
     handleMouseScroll(): void {
         this.canvasEl.addEventListener('wheel', (e: WheelEvent) => {
-            if (e.deltaY > 0) {
-                this.BaseCanvas.rotateCanvas(false, degree * (rotationDelta / scrollDivider));
+            this.rightScrollLimit = this.mouseScroll <= this.rightLimit;
+            this.leftScrollLimit = this.mouseScroll >= this.leftLimit;
+
+            if (e.deltaY > 0 && this.rightScrollLimit) {
+                this.BaseCanvas.rotateCanvas(false, degree * (rotationDelta / (scrollDivider * 2)));
                 this.mouseScroll += 1;
                 this.rotationStack += 1;
                 eventBus.emit(SCROLL_EVENTS.SCROLL_CHANGED, this.mouseScroll);
-            } else if (e.deltaY < 0) {
-                this.BaseCanvas.rotateCanvas(true, degree * (rotationDelta / scrollDivider));
+            } else if (e.deltaY < 0 && this.leftScrollLimit) {
+                this.BaseCanvas.rotateCanvas(true, degree * (rotationDelta / (scrollDivider * 2)));
                 this.mouseScroll -= 1;
                 this.rotationStack -= 1;
                 eventBus.emit(SCROLL_EVENTS.SCROLL_CHANGED, this.mouseScroll);
@@ -56,14 +81,16 @@ class ScrollEventHandler {
             const moveX = Math.floor(touch.clientX);
             this.touchDirection = moveX - touchStartX;
 
-            if (this.touchDirection < 0) {
-                // console.log('캔버스 돌아감');
-                this.BaseCanvas.rotateCanvas(false, degree * (rotationDelta / scrollDivider));
+            this.rightScrollLimit = this.touchMove <= this.rightLimit;
+            this.leftScrollLimit = this.touchMove >= this.leftLimit;
+
+            if (this.touchDirection < 0 && this.rightScrollLimit) {
+                this.BaseCanvas.rotateCanvas(false, degree * (rotationDelta / (scrollDivider * 2)));
                 this.touchMove += 1;
                 this.rotationStack += 1;
                 eventBus.emit(SCROLL_EVENTS.TOUCH_CHANGED, this.touchMove);
-            } else if (this.touchDirection > 0) {
-                this.BaseCanvas.rotateCanvas(true, degree * (rotationDelta / scrollDivider));
+            } else if (this.touchDirection > 0 && this.leftScrollLimit) {
+                this.BaseCanvas.rotateCanvas(true, degree * (rotationDelta / (scrollDivider * 2)));
                 this.touchMove -= 1;
                 this.rotationStack -= 1;
                 eventBus.emit(SCROLL_EVENTS.TOUCH_CHANGED, this.touchMove);
