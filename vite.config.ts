ce1f5adb6088fa0f1ff9b0e97e310e34e372a03a
Sunwoo Ha <tjsdn811@gmail.com>
2025-11-09 00:24:29 +0900

    test: deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,16 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const repo = 'NeuralNetwork-Visualization';
+const branch = process.env.GITHUB_REF_NAME || '';
+let base = `/${repo}/`;
+
+if (branch.startsWith('feat/')) {
+    base = `/${repo}/${branch}/`;
+}
+
 export default defineConfig({
+    base: base,
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     },
